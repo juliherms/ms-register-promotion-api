73860d6b8c4e4a0ed5f43b6ddacefb7636c0952b
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-01-02 19:32:33 -0300

    code review

--- src/main/kotlin/com/github/juliherms/register/controller/PromotionsController.kt
+++ src/main/kotlin/com/github/juliherms/register/controller/PromotionsController.kt
@@ -4,8 +4,6 @@ import com.github.juliherms.register.model.Promotion
 import com.github.juliherms.register.service.PromotionService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
-import java.util.concurrent.ConcurrentHashMap
-
 
 /**
  * This class responsible to provide promotions endpoint
@@ -19,29 +17,26 @@ class PromotionsController {
 
     @GetMapping("/{id}")
     fun getById(@PathVariable id: Long): Promotion? {
-        return service.promotions[id];
+        return service.getById(id)
      }
 
     @PostMapping()
     fun create(@RequestBody promotion: Promotion) {
-        service.promotions[promotion.id] = promotion
+        service.create(promotion)
     }
 
     @DeleteMapping("/{id}")
     fun delete (@PathVariable id: Long) {
-        service.promotions.remove(id)
+        service.remove(id)
     }
 
     @PutMapping("/{id}")
     fun update (@PathVariable id:Long, @RequestBody promotion:Promotion) {
-        service.promotions.remove(id)
-        service.promotions[id] = promotion
+        service.update(id,promotion)
     }
 
     @GetMapping("")
     fun listAll(@RequestParam(required = false, defaultValue = "") description: String) =
+        service.filter(description)
 
-        service.promotions.filter {
-            it.value.description.contains(description,true)
-        }.map (Map.Entry<Long,Promotion>::value).toList()
 }
--- src/main/kotlin/com/github/juliherms/register/service/PromotionService.kt
+++ src/main/kotlin/com/github/juliherms/register/service/PromotionService.kt
@@ -2,6 +2,7 @@ package com.github.juliherms.register.service
 
 import com.github.juliherms.register.model.Promotion
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Description
 import org.springframework.stereotype.Component
 import java.util.concurrent.ConcurrentHashMap
 
@@ -23,4 +24,22 @@ class PromotionService {
     fun create(promotion: Promotion) {
         promotions[promotion.id] = promotion
     }
+
+    fun getById(id: Long): Promotion? {
+        return promotions[id]
+    }
+
+    fun update(id: Long, promotion:Promotion){
+        promotions.remove(id)
+        promotions[id] = promotion
+    }
+
+    fun remove (id: Long) {
+        promotions.remove(id)
+    }
+
+    fun filter (description: String)  =
+            promotions.filter {
+                it.value.description.contains(description,true)
+            }.map (Map.Entry<Long,Promotion>::value).toList()
 }
